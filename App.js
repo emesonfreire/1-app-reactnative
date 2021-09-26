17b205aa37b409e9c1a200985badca5278b9fc52
Emeson Freire <67517204+emesonfreire@users.noreply.github.com>
2021-09-26 20:34:10 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Title from './components/Title/'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Olá Emeson, bem vindo</Text>
+      <Title/>
+      
       <StatusBar style="auto" />
     </View>
   );
